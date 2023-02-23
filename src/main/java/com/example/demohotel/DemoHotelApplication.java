9a550cfd23ff36193712c1da4489576f11902d55
Abduganiyev <mr.show050@gmail.com>
2023-02-23 21:32:17 +0500

    first

---
+++ src/main/java/com/example/demohotel/DemoHotelApplication.java
@@ -0,0 +1,14 @@
+package com.example.demohotel;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+public class DemoHotelApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DemoHotelApplication.class, args);
+    }
+
+}
