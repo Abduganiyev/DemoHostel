66406fa6014d694543ff91d1d08c7ef208aae525
Abduganiyev <mr.show050@gmail.com>
2023-02-26 16:06:55 +0500

    second

---
+++ src/main/java/com/example/demohotel/controller/HostelController.java
@@ -0,0 +1,11 @@
+package com.example.demohotel.controller;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/api/v1/hostels")
+public class HostelController {
+}
