db17a805d649b0c5b42f57c0c3b67636e2b9cca4
Abduganiyev <mr.show050@gmail.com>
2023-02-26 16:03:34 +0500

    second

---
+++ src/main/java/com/example/demohotel/confing/SwaggerConfig.java
@@ -0,0 +1,40 @@
+package com.example.demohotel.confing;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import springfox.documentation.builders.ApiInfoBuilder;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.*;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spi.service.contexts.SecurityContext;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+
+@Configuration
+@EnableSwagger2
+public class SwaggerConfig {
+
+    @Bean
+    public Docket api(){
+        return new Docket(DocumentationType.SWAGGER_2)
+                .select()
+                .apis(RequestHandlerSelectors.basePackage("com.example.demohotel"))
+                .paths(PathSelectors.any())
+                .build().apiInfo(metadata());
+    }
+
+    private ApiInfo metadata() {
+        return new ApiInfoBuilder().title("Web App")
+                .description("This project is a real website")
+                .license("Apache version 2.0")
+                .version("1.1.0")
+                .licenseUrl("https://www.apache.org/licenses/LICENSE-2.0.html")
+                .contact(new Contact("Digital One", "www.group.uz", "azizovmuhammaduz@gmail.com"))
+                .build();
+    }
+}
