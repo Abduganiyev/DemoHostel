014ed4067427b097e69951a6f2586432f0ff1988
Abduganiyev <mr.show050@gmail.com>
2023-02-27 15:00:52 +0500

    init

--- src/main/java/com/example/demohotel/dto/HostelCreateDto.java
+++ src/main/java/com/example/demohotel/dto/HostelCreateDto.java
@@ -1,7 +1,12 @@
 package com.example.demohotel.dto;
 
-import javax.persistence.Column;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class HostelCreateDto {
     private String name;
     private Long room_counts;
--- src/main/java/com/example/demohotel/service/impl/HostelServiceImpl.java
+++ src/main/java/com/example/demohotel/service/impl/HostelServiceImpl.java
@@ -2,15 +2,19 @@ package com.example.demohotel.service.impl;
 
 import com.example.demohotel.dto.HostelCreateDto;
 import com.example.demohotel.entity.Hostel;
+import com.example.demohotel.repository.HostelRepository;
 import com.example.demohotel.service.HostelService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
+import java.sql.DataTruncation;
 import java.util.List;
 
 @Service
 @RequiredArgsConstructor
 public class HostelServiceImpl implements HostelService {
+    private final HostelRepository hostelRepository;
+
     @Override
     public List<Hostel> findAll() {
         return null;
@@ -28,7 +32,15 @@ public class HostelServiceImpl implements HostelService {
 
     @Override
     public Hostel saveHostel(HostelCreateDto dto) {
-        return null;
+        Hostel hostel = new Hostel(dto.getName(),
+                dto.getRoom_counts(),
+                dto.getFree_room_counts(),
+                dto.getFull_room_counts(),
+                dto.isPrestige(),
+                dto.getAddress(),
+                dto.getFormed());
+        Hostel save = hostelRepository.save(hostel);
+        return save;
     }
 
     @Override
