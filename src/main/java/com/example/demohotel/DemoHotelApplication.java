e05078f990baca23fd67743f31136beadeed9efc
Abduganiyev <mr.show050@gmail.com>
2023-03-15 03:02:34 +0500

    new init

--- src/main/java/com/example/demohotel/DemoHotelApplication.java
+++ src/main/java/com/example/demohotel/DemoHotelApplication.java
@@ -1,14 +1,26 @@
 package com.example.demohotel;
 
+import com.example.demohotel.dto.RoleCreateDto;
+import com.example.demohotel.service.RoleService;
+import com.example.demohotel.service.impl.RoleServiceImpl;
+import lombok.AllArgsConstructor;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
-public class DemoHotelApplication {
+@AllArgsConstructor
+public class DemoHotelApplication implements CommandLineRunner{
+    private final RoleService roleService;
 
     public static void main(String[] args) {
         SpringApplication.run(DemoHotelApplication.class, args);
     }
 
+    @Override
+    public void run(String... args) throws Exception {
+
+        roleService.saveRole(new RoleCreateDto("Xodim"));
+        roleService.saveRole(new RoleCreateDto("Istemolchi"));
+    }
 }
--- src/main/java/com/example/demohotel/controller/RoleController.java
+++ src/main/java/com/example/demohotel/controller/RoleController.java
@@ -15,19 +15,21 @@ public class RoleController {
 
     private final RoleService roleService;
 
-    @PostMapping
+/*    @PostMapping
     public Role save(@RequestBody RoleCreateDto dto) {
         return roleService.saveRole(dto);
-    }
+    }*/
 
     @GetMapping
     public List<Role> findAll() {
         return roleService.findAll();
     }
+/*
 
     @DeleteMapping("/{role_id}")
     public String remove(@PathVariable("role_id") Long roleId) {
         return roleService.removeRoleById(roleId);
     }
+*/
 
 }
